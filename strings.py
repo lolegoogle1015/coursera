eb99d3af1605558a9af9e0d209482194ac186ae5
lolegoogle <hryshchuk1998@gmail.com>
2020-03-31 02:51:15 +0300

    Encode and decode of strings

--- strings.py
+++ strings.py
@@ -1,46 +1,20 @@
-#String Methods
-quote = """Болтовня ничего не стоит. Покажите мне код.
+#Bytes
+#b-літерал для оголошення байтової стрічки
 
-Linus Torvalds
-"""
-print(quote.count("o"))
+example_bytes = b'hello'
+print(type(example_bytes))
 
-print("ukraine".capitalize())
+for element in example_bytes:
+    print(element)
 
-print("2020".isdigit())
+#example_bytes = b"Привіт"
+example_string = 'Привіт'
+print(type(example_string))
+print(example_string)
 
-#"in" operator
+encoded_string = example_string.encode(encoding="utf-8")
+print(encoded_string)
+print(type(encoded_string))
 
-print("3.14" in "Pi number = 3.1415926")
-
-print("Oleh" in "Andriy")
-
-#"for in"
-example_string = "Hi"
-for letter in example_string:
-    print("Letter", letter)
-
-str = bool("asdad")
-print(str)
-
-#Strings formating
-template = "%s - головна перевага програміста. (%s)(%d)" % ("Лінь", "Larry Wall",190)
-print(template)
-
-print("{} не брешуть, але {} користуються формулами. ({})".format("Цифри", "Брехуни", "Robert A.Heinlein"))
-print("{num} не брешуть, але {people} користуються формулами. ({author})".format(num="Цифри", people="Брехуни", author="Robert A.Heinlein"))
-
-#f-strings Python3.6
-
-subject = "optimization"
-author = "Donald Knuth"
-
-print(f"Передчасна {subject} - корінь всіх бід. ({author})")
-
-num = 8
-print(f"Binary: {num:#b}")
-
-num = 2/3
-print(num)
-
-print(f"{num:.3f}")
+decoded_string = encoded_string.decode()
+print(decoded_string)
