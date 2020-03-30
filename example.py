3daf77bbd91cf6be097e58eb78867cf16c8fc5e3
lolegoogle <hryshchuk1998@gmail.com>
2020-03-31 01:39:34 +0300

    Data types. Types convertation

--- example.py
+++ example.py
@@ -1,10 +1,20 @@
 print("Hello World")
 
-num = 1
-num = 2
-num = "Hello World"
+num = 1 #int
+num = 2 #int
+num = "Hello World" #string
+num = 100_000_000.0 #it`s right)
 
 snake_case = "111"
 
 for i in range(4):
     print(i)
+print(type(num))
+print(num)
+
+#конвертація типів
+num = int(num)
+print(type(num))
+print(num)
+
+#Module @decimal, @fractions
