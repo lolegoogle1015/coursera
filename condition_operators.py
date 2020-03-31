9828bd2ec13924b4cf736744a6534c7fbc413b81
lolegoogle <hryshchuk1998@gmail.com>
2020-03-31 03:20:21 +0300

    pass/break/continue

--- condition_operators.py
+++ condition_operators.py
@@ -1,57 +1,25 @@
-company = "my.com"
-if "my" in company or company.endswith(".com"):
-    print("Condition is completed")
+#pass
+for i in range(100):
+    pass
 
-company = "google.com"
-if "my" in company:
-    print("Умова виконана!")
-else:
-    print("Умова не виконана!")
-
-company = "google.com"
-if "my" in company:
-    print("Підстрічка не знайдена")
-elif "google" in company:
-    print("Підстрічка google знайдена")
-else:
-    print("Підстрічка не знайдена!")
-
-#Тернарний оператор
-
-score_1 = 5
-score_2 = 0
-winner = "Argentina" if score_1 > score_2 else "Jamaica"
-print(winner)
-
-#while
-
-i = 0
-
-while i<100:
-    i +=1
-
-print(i)
-
-#for_in
-
-name = "Alex"
-for letter in name:
-    print(letter)
-
-for i in range(3):
-    print(i)
-
+#break
 result = 0
-
-for i in range(101):
-    result += i
+while True:
+    result +=1
+    if result >= 100:
+        break
 print(result)
 
-for i in range(5,8):
+for i in range(10):
+    if i == 5:
+        break
     print(i)
 
-for i in range(1, 10, 2):
-    print(i)
+#continue
+result = 0
 
-for i in range(10, 5, -1):
-    print(i)
+for i in range(10):
+    if i % 2 != 0:
+        continue
+    result += i
+print(result)
